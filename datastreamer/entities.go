bd99c1f6272b7229cf8a7cbc8835ecd100e62ed1
Toni Ramírez <58293609+ToniRamirezM@users.noreply.github.com>
2023-09-15 13:09:49 +0200

    entities refactor (#21)
    * entities refactor
    
    * allow getting log level

--- datastreamer/entities.go
+++ datastreamer/entities.go
@@ -1,9 +1,12 @@
 package datastreamer
 
 import (
+	"encoding/binary"
 	"fmt"
 	"reflect"
 	"unsafe"
+
+	"github.com/0xPolygonHermez/zkevm-data-streamer/log"
 )
 
 type EntityDefinition struct {
@@ -13,15 +16,49 @@ type EntityDefinition struct {
 }
 
 func (e EntityDefinition) toString(entity []byte) string {
+	var bigEndian = (*(*[2]uint8)(unsafe.Pointer(&[]uint16{1}[0])))[0] == 0
 	obj := reflect.NewAt(e.Definition, unsafe.Pointer(&entity[0]))
 	val := obj.Elem()
 	stringValue := ""
+	accumulatedSize := 0
 
 	for i := 0; i < val.NumField(); i++ {
-		stringValue += fmt.Sprintf(val.Type().Field(i).Name + ": " + fmt.Sprintf("%v", val.Field(i).Interface()))
+		name := val.Type().Field(i).Name
+		value := ""
+
+		if val.Type().Field(i).Type.Kind() == reflect.Uint64 {
+			if bigEndian {
+				value = fmt.Sprintf("%v", binary.BigEndian.Uint64(entity))
+			} else {
+				value = fmt.Sprintf("%v", binary.LittleEndian.Uint64(entity))
+			}
+			accumulatedSize += 8
+		} else if val.Type().Field(i).Type.Kind() == reflect.Uint32 {
+			if bigEndian {
+				value = fmt.Sprintf("%v", binary.BigEndian.Uint32(entity))
+			} else {
+				value = fmt.Sprintf("%v", binary.LittleEndian.Uint32(entity))
+			}
+			accumulatedSize += 4
+		} else if val.Type().Field(i).Type.Kind() == reflect.Uint8 {
+			value = fmt.Sprintf("%v", entity[0])
+			accumulatedSize += 1
+		} else if val.Type().Field(i).Type.Kind() == reflect.Slice {
+			if len(entity) > accumulatedSize {
+				value = fmt.Sprintf("%v", entity[accumulatedSize:len(entity)-accumulatedSize])
+			} else {
+				value = "[slice]"
+			}
+		} else {
+			value = fmt.Sprintf("%v", val.Field(i).Interface())
+		}
+
+		stringValue += fmt.Sprintf(name + ": " + value)
 		if i < val.NumField()-1 {
 			stringValue += ", "
 		}
+
+		log.Debug(stringValue)
 	}
 	return stringValue
 }
--- tool/db/types.go
+++ tool/db/types.go
@@ -1,9 +1,11 @@
 package db
 
 import (
+	"encoding/binary"
 	"unsafe"
 
 	"github.com/0xPolygonHermez/zkevm-data-streamer/datastreamer"
+	"github.com/0xPolygonHermez/zkevm-data-streamer/log"
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -18,35 +20,61 @@ const (
 
 // L2Block represents a L2 block
 type L2Block struct {
-	BatchNumber    uint64
-	L2BlockNumber  uint64
-	Timestamp      int64
-	GlobalExitRoot common.Hash
-	Coinbase       common.Address
+	BatchNumber    uint64         // 8 bytes
+	L2BlockNumber  uint64         // 8 bytes
+	Timestamp      int64          // 8 bytes
+	GlobalExitRoot common.Hash    // 32 bytes
+	Coinbase       common.Address // 20 bytes
 }
 
 // Encode returns the encoded L2Block as a byte slice
 func (b L2Block) Encode() []byte {
-	const size = int(unsafe.Sizeof(L2Block{}))
-	return (*(*[size]byte)(unsafe.Pointer(&b)))[:]
+	log.Debugf("BatchNumber: %v", b.BatchNumber)
+	var bigEndian = (*(*[2]uint8)(unsafe.Pointer(&[]uint16{1}[0])))[0] == 0
+
+	bytes := make([]byte, 0)
+	if bigEndian {
+		bytes = binary.BigEndian.AppendUint64(bytes, b.BatchNumber)
+		bytes = binary.BigEndian.AppendUint64(bytes, b.L2BlockNumber)
+		bytes = binary.BigEndian.AppendUint64(bytes, uint64(b.Timestamp))
+	} else {
+		bytes = binary.LittleEndian.AppendUint64(bytes, b.BatchNumber)
+		bytes = binary.LittleEndian.AppendUint64(bytes, b.L2BlockNumber)
+		bytes = binary.LittleEndian.AppendUint64(bytes, uint64(b.Timestamp))
+	}
+	bytes = append(bytes, b.GlobalExitRoot.Bytes()...)
+	bytes = append(bytes, b.Coinbase.Bytes()...)
+
+	log.Debugf("Encoded L2Block: %v", bytes)
+
+	return bytes
 }
 
 // L2Transaction represents a L2 transaction
 type L2Transaction struct {
-	BatchNumber                 uint64
-	EffectiveGasPricePercentage uint8
-	IsValid                     uint8
-	EncodedLength               uint32
+	BatchNumber                 uint64 // 8 bytes
+	EffectiveGasPricePercentage uint8  // 1 byte
+	IsValid                     uint8  // 1 byte
+	EncodedLength               uint32 // 4 bytes
 	Encoded                     []byte
 }
 
 // Encode returns the encoded L2Transaction as a byte slice
 func (l L2Transaction) Encode() []byte {
-	const size = int(unsafe.Sizeof(L2Transaction{}))
-	return (*(*[size]byte)(unsafe.Pointer(&l)))[:]
-}
-
-// DecodeL2Transaction decodes a byte slice into a L2Transaction
-func DecodeL2Transaction(data []byte) L2Transaction {
-	return *(*L2Transaction)(unsafe.Pointer(&data[0]))
+	var bigEndian = (*(*[2]uint8)(unsafe.Pointer(&[]uint16{1}[0])))[0] == 0
+	bytes := make([]byte, 0)
+	if bigEndian {
+		bytes = binary.BigEndian.AppendUint64(bytes, l.BatchNumber)
+	} else {
+		bytes = binary.LittleEndian.AppendUint64(bytes, l.BatchNumber)
+	}
+	bytes = append(bytes, l.EffectiveGasPricePercentage)
+	bytes = append(bytes, l.IsValid)
+	if !bigEndian {
+		bytes = binary.BigEndian.AppendUint32(bytes, l.EncodedLength)
+	} else {
+		bytes = binary.LittleEndian.AppendUint32(bytes, l.EncodedLength)
+	}
+	bytes = append(bytes, l.Encoded...)
+	return bytes
 }
