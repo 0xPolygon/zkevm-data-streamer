a831bf625ef21c98ec316b6a429c4f5f8a2f580e
Toni Ramírez <toni@polygon.technology>
2023-10-11 22:50:42 +0200

    fix entity toString and test

--- datastreamer/entities.go
+++ datastreamer/entities.go
@@ -1,6 +1,7 @@
 package datastreamer
 
 import (
+	"encoding/binary"
 	"fmt"
 	"reflect"
 	"unsafe"
@@ -13,16 +14,28 @@ type EntityDefinition struct {
 	Definition reflect.Type
 }
 
+// ToString is only meant for testing purposes
+func (e EntityDefinition) ToString(entity []byte) string {
+	return e.toString(entity)
+}
+
 func (e EntityDefinition) toString(entity []byte) string {
 	obj := reflect.NewAt(e.Definition, unsafe.Pointer(&entity[0]))
 	val := obj.Elem()
 	stringValue := ""
+	accumulatedLength := 0
 	for i := 0; i < val.NumField(); i++ {
 		name := val.Type().Field(i).Name
 		value := ""
 
 		if val.Type().Field(i).Type.Kind() == reflect.Slice {
-			value = "[slice]"
+			value = string(entity[accumulatedLength:])
+		} else if val.Type().Field(i).Type.Kind() == reflect.Uint8 {
+			value = fmt.Sprintf("%v", entity[accumulatedLength])
+		} else if val.Type().Field(i).Type.Kind() == reflect.Uint32 {
+			value = fmt.Sprintf("%v", binary.LittleEndian.Uint32(entity[accumulatedLength:accumulatedLength+4]))
+		} else if val.Type().Field(i).Type.Kind() == reflect.Uint64 {
+			value = fmt.Sprintf("%v", binary.LittleEndian.Uint64(entity[accumulatedLength:accumulatedLength+8]))
 		} else {
 			value = fmt.Sprintf("%v", val.Field(i).Interface())
 		}
@@ -31,6 +44,8 @@ func (e EntityDefinition) toString(entity []byte) string {
 		if i < val.NumField()-1 {
 			stringValue += ", "
 		}
+
+		accumulatedLength += int(val.Type().Field(i).Type.Size())
 	}
 	return stringValue
 }
--- datastreamer/entities_test.go
+++ datastreamer/entities_test.go
@@ -1,25 +1,40 @@
 package datastreamer_test
 
-/*
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/0xPolygonHermez/zkevm-data-streamer/datastreamer"
+	"github.com/stretchr/testify/assert"
+)
+
 func TestEntityToString(t *testing.T) {
-	l2Transaction := db.L2Transaction{
-		BatchNumber:                 1,                     // 8 bytes
+	l2Transaction := datastreamer.L2Transaction{
 		EffectiveGasPricePercentage: 128,                   // 1 byte
 		IsValid:                     1,                     // 1 byte
 		EncodedLength:               5,                     // 4 bytes
 		Encoded:                     []byte{1, 2, 3, 4, 5}, // 5 bytes
 	}
 
+	l2Transaction2 := datastreamer.L2Transaction{
+		EffectiveGasPricePercentage: 255,                                   // 1 byte
+		IsValid:                     1,                                     // 1 byte
+		EncodedLength:               10,                                    // 4 bytes
+		Encoded:                     []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, // 10bytes
+	}
+
 	entityDef := datastreamer.EntityDefinition{
 		Name:       "L2Transaction",
-		StreamType: datastreamer.StreamType(db.StreamTypeSequencer),
-		Definition: reflect.TypeOf(db.L2Transaction{}),
+		StreamType: datastreamer.StreamType(datastreamer.StreamTypeSequencer),
+		Definition: reflect.TypeOf(datastreamer.L2Transaction{}),
 	}
 
 	stringValue := entityDef.ToString(l2Transaction.Encode())
-	expected := "BatchNumber: 1, EffectiveGasPricePercentage: 128, IsValid: 1, EncodedLength: 5, Encoded: [slice]"
-
+	expected := fmt.Sprintf("EffectiveGasPricePercentage: %d, IsValid: %d, EncodedLength: %d, Encoded: %s", l2Transaction.EffectiveGasPricePercentage, l2Transaction.IsValid, l2Transaction.EncodedLength, l2Transaction.Encoded)
 	assert.Equal(t, expected, stringValue)
 
+	stringValue = entityDef.ToString(l2Transaction2.Encode())
+	expected = fmt.Sprintf("EffectiveGasPricePercentage: %d, IsValid: %d, EncodedLength: %d, Encoded: %s", l2Transaction2.EffectiveGasPricePercentage, l2Transaction2.IsValid, l2Transaction2.EncodedLength, l2Transaction2.Encoded)
+	assert.Equal(t, expected, stringValue)
 }
-*/
