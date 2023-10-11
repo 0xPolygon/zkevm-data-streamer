bd2a4afdd812eb3f118472edb33bb60e34ec974a
Toni Ramírez <toni@polygon.technology>
2023-10-11 23:25:40 +0200

    test entity

--- datastreamer/entities.go
+++ datastreamer/entities.go
@@ -6,7 +6,6 @@ import (
 	"reflect"
 	"unsafe"
 
-	"github.com/0xPolygonHermez/zkevm-data-streamer/log"
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -49,7 +48,6 @@ func (e EntityDefinition) toString(entity []byte) string {
 		}
 
 		accumulatedLength += int(val.Type().Field(i).Type.Size())
-		log.Infof("AccumulatedLength: %v", accumulatedLength)
 	}
 	return stringValue
 }
