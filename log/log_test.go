1cca17949e3c65d1a34bad5a87a595e21bf4d298
Stefan Negovanović <stefan@ethernal.tech>
2024-07-29 20:20:44 +0200

    Introduce SonarCloud with code coverage

--- log/log_test.go
+++ log/log_test.go
@@ -5,15 +5,15 @@ import (
 )
 
 func TestLogNotInitialized(t *testing.T) {
-	Info("Test log.Info", " value is ", 10)
+	Info("Test log.Info value is ", 10)
 	Infof("Test log.Infof %d", 10)
 	Infow("Test log.Infow", "value", 10)
 	Debugf("Test log.Debugf %d", 10)
-	Error("Test log.Error", " value is ", 10)
+	Error("Test log.Error value is ", 10)
 	Errorf("Test log.Errorf %d", 10)
-	Errorw("Test log.Errorw", "value", 10)
+	Errorw("Test log.Errorw value", 10)
 	Warnf("Test log.Warnf %d", 10)
-	Warnw("Test log.Warnw", "value", 10)
+	Warnw("Test log.Warnw value", 10)
 }
 
 func TestLog(t *testing.T) {
@@ -25,13 +25,13 @@ func TestLog(t *testing.T) {
 
 	Init(cfg)
 
-	Info("Test log.Info", " value is ", 10)
+	Info("Test log.Info value is ", 10)
 	Infof("Test log.Infof %d", 10)
 	Infow("Test log.Infow", "value", 10)
 	Debugf("Test log.Debugf %d", 10)
-	Error("Test log.Error", " value is ", 10)
+	Error("Test log.Error value is ", 10)
 	Errorf("Test log.Errorf %d", 10)
 	Errorw("Test log.Errorw", "value", 10)
 	Warnf("Test log.Warnf %d", 10)
-	Warnw("Test log.Warnw", "value", 10)
+	Warnw("Test log.Warnw value", 10)
 }
